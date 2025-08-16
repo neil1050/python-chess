1092935b2e416592bc03e4807f9e61cc6f801cc0
Neil Li <neil1050.com@gmail.com>
2025-08-16 19:51:27 +0100

    hotfix: add colour to arguments of piece class constructor in chess.py
    chess.py: add colour argument to constructor in piece class
    
              this has been done due to colours being a necessary part of
              chess, and being needed in the FEN reading section of board.

--- chess.py
+++ chess.py
@@ -7,7 +7,7 @@
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 class piece:
-    def __init__(self, boardIndex: int) -> None:
+    def __init__(self, boardIndex: int, colour: str) -> None:
         """Initialises a chess piece
 
         :self: piece - the piece type and its location
@@ -16,6 +16,7 @@ class piece:
         This constructor emulates placing a piece on a certain tile of the chess board
         """
         self.position = boardIndex
+        self.colour = colour
     
     def getMoves(self, obstacles: list[bool], sideLength: int = 8) -> list[int]:
         """Returns the valid moves of a piece
@@ -52,6 +53,7 @@ class board:
         :startingFen: str - the associated FEN code for the chess game
 
         This constructor takes a FEN code and parses it into a board object"""
+        # turns will be managed by the client
         self.boardPieces = []
     
     def checkMove(self, origin: int, target: int) -> bool:
