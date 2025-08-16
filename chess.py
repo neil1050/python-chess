48ea3ce5d497ab7c98aab2e6fcf0a07d84d2344e
Neil Li <neil1050.com@gmail.com>
2025-08-16 16:52:28 +0100

    update: add definitions to chess.py
    chess.py: add class piece, board
              add basic functions to piece:
                  __init__
                  getMoves
    
              add basic functions to board:
                  __init__
              add variables to board:
                  boardSideLength - the side length for a chess game
                  pieceMappings - allows adding new pieces
    
              this is to allow an easy base to add new pieces (hence the
              basic piece class)
              no functionality is available yet
    
    README: add new info about board indexing internally

--- chess.py
+++ chess.py
@@ -5,3 +5,41 @@
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+class piece:
+    def __init__(self, boardIndex: int) -> None:
+        """Initialises a chess piece
+
+        :self: piece - the piece type and its location
+        :boardIndex: int - where the piece is on the board
+        
+        This constructor emulates placing a piece on a certain tile of the chess board
+        """
+        self.position = boardIndex
+    
+    def getMoves(self, obstacles: list[bool], sideLength: int = 8) -> list[int]:
+        """Returns the valid moves of a piece
+
+        :self: piece - the piece type and its location
+        :obstacles: list[bool] - the potential blocking pieces
+        :sideLength: - the side length of the board
+        :return: list[int] - the squares the given piece can move on to
+
+        piece.getMoves takes a list of obstacles, and using the self.position variable, determines all valid moves for the piece (using sideLength as a bounding check)
+        """
+        return []
+
+class board:
+    boardSideLength = 8  # default chess board
+
+    pieceMappings = {
+            }
+    
+    def __init__(self, startingFen: str) -> None:
+        """Initialises a chess board
+
+        :self: board - the board the game will take place on
+        :startingFen: str - the associated FEN code for the chess game
+
+        This constructor takes a FEN code and parses it into a board object"""
+        pass
