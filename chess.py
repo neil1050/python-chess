6e40a2d8b5ec73dd91551acbd78e82b84fcd6b00
Neil Li <76211576+neil1050@users.noreply.github.com>
2025-09-14 14:04:33 +0100

    feat(chess.py): implement importMods
    importMods can now import outside modules and check if they have a
    manifest to read information from

--- chess.py
+++ chess.py
@@ -6,6 +6,9 @@
 #
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import importlib
+import json
+
 from pieces import king, queen, rook, bishop, knight, pawn
 
 # board class
@@ -330,3 +333,30 @@ class board:
         else:
             self.playerTurn = "w"
         return True  # if we have reached the end of the function, we return True
+
+importedMods = {}
+
+def importMods(modsList: list[str] | None = None):
+    global importedMods
+
+    if modsList is None:
+        # due to mutable default arguments
+        modsList = []
+    
+    for mod in modsList:
+        if mod in importedMods.keys():
+            # mod has already been imported
+            continue
+        try:
+            importedMod = importlib.import_module(mod)
+        except ModuleNotFoundError:
+            print(f"Failed to import {mod}")
+            continue
+        
+        try:
+            importedMods[mod] = {"class": importedMod, "manifest": importedMod.manifest}
+        except AttributeError:
+            print(f"Manifest is unavailable for {mod}, skipping...")
+            continue
+        finally:
+            del importedMod
